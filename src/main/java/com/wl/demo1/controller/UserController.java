3e0f568014980b0131bbe3924fc8ef5afdb43c54
wcl2184 <wcl2184@163.com>
2018-04-09 23:38:58 +0800

    4444

--- src/main/java/com/wl/demo1/controller/UserController.java
+++ src/main/java/com/wl/demo1/controller/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
         user.setName("name"+userId);
         user.setCreateTime(new Date());
         System.out.println("xxxx");
-        System.out.println("第二个打印输出123ee77771231233333");
+        System.out.println("第二个打印输出123ee777712312333331212eeryyy");
         return user;
     }
 }
